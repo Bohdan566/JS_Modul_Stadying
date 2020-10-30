01937ec74b91df536e98ad5661ab42a4386eb221
Bohdan566 <bohdan55566@gmail.com>
2020-10-30 20:50:53 +0200

    Stadyin_1

---
+++ JSStadying_1.js
@@ -0,0 +1,222 @@
+
+// Завдання_1
+
+// let str = "Привіт";
+// let num = 123;
+// let flag = true;
+// let txt = "true";
+
+// console.log(typeof(str));
+// console.log(typeof(num));
+// console.log(typeof(flag));
+// console.log(typeof(txt));
+
+// Завдання_2
+
+// let a1 = 5 + 3;
+// let a2 = 5 - 3;
+// let a3 = 5 * 3;
+// let a4 = 5 / 3;
+// let a5 = 5 % 3;
+
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+// console.log(a5);
+
+// Завдання_3
+
+// let a6 = 5 % 3;
+// let a7 = 3 % 5;
+// let a8 = 5 + "3";
+// let a9 = "5" - 3;
+// let a10 = 75 + "кг";
+
+// console.log(a6);
+// console.log(a7);
+// console.log(a8);
+// console.log(a9);
+// console.log(a10);
+
+// Завдання_4
+
+// let height = 28;
+// let weight = 10
+// let s = height * weight;
+
+// console.log(s);
+
+// Завдання_5
+
+// let height = 10;
+// let dc = 4;
+// let v = 0.25 * 3.14 * 10**2
+
+// console.log(v);
+
+// Завдання_6
+
+// let n = 3;
+// let m = 4;
+// let k
+// let k1
+
+// k1 = Math.pow(3, 2) + Math.pow(3, 2);
+// k = Math.sqrt(k1, 2);
+
+// console.log(k);
+
+// Завдання_7
+
+// let str = "Hello World";
+
+// console.log(str);
+// document.write(str);
+// alert(str);
+
+// Завдання_8
+
+// let fullName = prompt("Your full name");
+// let age = prompt("Your age");
+// let hobby = prompt("Your hobby");
+
+// alert(fullName);
+// alert(age);
+// alert(hobby);
+
+// Завдання_9
+
+// let str1 = "Хто ";
+// let str2 = "ти ";
+// let str3 = "такий?";
+// let concatenation = str1 + str2 + str3;
+
+// console.log(concatenation);
+
+// Завдання_10
+
+
+// let str = "20";
+// let a = 5;
+
+// document.write(str + a + "<br>");
+// document.write(str - a + "<br>");
+// document.write(str * 2 + "<br>");
+// document.write(str / 2 + "<br>");
+
+// Завдання_11
+
+// console.log(parseInt("3.14"));
+// console.log(parseInt("-7.875"));
+// console.log(parseInt("435"));
+// console.log(parseInt("Вася"));
+
+// Завдання_12
+
+// let str = prompt("Enter something", "ho-ho");
+
+// console.log(str);
+
+// Завдання_13
+
+// let num1 = +prompt("Введіть перше число", "123");
+// let num2 = +prompt("Введіть друге число", "123");
+
+// alert(num1 + num2);
+
+// Завдання_14
+
+// let firstName = prompt("Your first name");
+// let lastName = prompt("Your last name");
+// let age = prompt("Your age");
+// let fullStr = "Доброго вечора " + firstName + " " + lastName + " мої вітання що вам " + age;
+
+// alert(fullStr);
+
+// Завдання_1
+
+// let number1 = parseInt(prompt("Введіть перше число"));
+// let number2 = parseInt(prompt("Введіть друге число"));
+// let number3 = parseInt(prompt("Введіть третє число"));
+
+// if (number1 < number2 && number1 < number3) {
+//     if (number2 < number3) {
+//         console.log(number1, number2, number3)
+//     }else{
+//         console.log(number1, number3, number2)
+//     }
+// } else if (number2 < number1 && number2 < number3) {
+//     if (number1 < number3) {
+//         console.log(number2, number1, number3);
+//     } else {
+//         console.log(number2, number3, number1);
+//     }
+// } else if (number3 < number1 && number3 < number2) {
+//     if(number1 < number2) {
+//         console.log(number3, number1, number2);
+//     } else {
+//         console.log(number3, number2, number1);
+//     }
+// }
+
+// Завдання_2
+
+// let var1 = prompt("виберіть колір (green, yellow, red)");
+
+// let color1 = "green";
+// let color2 = "yellow";
+// let color3 = "red";
+
+// if ( color1 == var1){
+//     console.log("Ви можете перейти дорогу");
+// } else if ( color2 == var1) {
+//     console.log("Почикай");
+// } else if ( color3 == var1) {
+//     console.log("Стій");
+// } else {
+//     console.log("Абрака-даб-ра!!!")
+// }
+
+// Завдання_3
+
+// let var1 = prompt("виберіть колір (green, yellow, red)");
+// let var2 = confirm("Чи бачиш ти машину на дорозі?");
+
+// let color1 = "green";
+// let color2 = "yellow";
+// let color3 = "red";
+
+// if ( color1 == var1 && var2 == false){
+//     console.log("Ви можете перейти дорогу");
+// } else if ( color2 == var1 && var2 == false) {
+//     console.log("Почикай");
+// } else if ( color3 == var1 && var2 == false) {
+//     console.log("Стій");
+// } else if ( color1 == var1 && var2 == true){
+//     console.log("Почикай поки порушник проїде");
+// } else if ( color2 == var1 && var2 == true){
+//     console.log("Всеодно чикай");
+// } else if ( color3 == var1 && var2 == true){
+//     console.log("Стій і чикай!!!");
+// } else {
+//     console.log("Абрака-даб-ра!!!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
